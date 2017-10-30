7f557ab847027ddb3106152cf118d195bde83349
rogers1lt <rogers1lt@live.com>
2017-10-30 12:59:01 -0700

    Added pseudocode for binary search

--- CS510_WebSec_P1.py
+++ CS510_WebSec_P1.py
@@ -19,3 +19,13 @@
 # http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5ba-z%5d.*%2F)%2F%2F
 
 testpwd = "t3sT"
+
+full_range = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+#Binary Search
+# 1. Is the character a number or capital?
+#	If yes is it a number?
+#		if yes then split the number range to find
+#		if no then split the capital letter range to find
+#	If no then split the lowercase letter range to find
+#
